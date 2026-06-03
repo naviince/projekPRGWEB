f730d1fac6d5745015783649c348d98301ce7b74
Fikri Sunanta <fikrimhmmd006@gmail.com>
2026-06-03 23:49:36 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,16 @@
+<?php
+
+$serverName = "LAPTOP-FB8R0UH0";
+
+$connectionOptions = array(
+    "Database" => "SpotLight",
+    "TrustServerCertificate" => true
+);
+
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+if (!$conn) {
+    die(print_r(sqlsrv_errors(), true));
+}
+
+?>
