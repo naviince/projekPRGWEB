6ba915f6d83b2aaaaebbd991db8a66ec43b2c9ac
alifahnada28 <alifahnada28@gmail.com>
2026-07-20 17:58:28 +0700

    ew

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$serverName = "ELVINA-PARAMITA";
+$serverName = "LAPTOP-EEPS0DEJ\ALSQLSERVER";
 $connectionOptions = array(
     "Database" => "SpotLight",
     "TrustServerCertificate" => true
